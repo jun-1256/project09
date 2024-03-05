0e1da7466b0e32d90dfda5a15c4e7384d17fd4e5
jun-1256 <160621341+jun-1256@users.noreply.github.com>
2024-03-05 21:28:34 +0900

    Update multidimensional_array.java
    7行目の//(ダブルクオーテーション)がズレていたため修正。

--- TestProject09/src/multidimensional_array.java
+++ TestProject09/src/multidimensional_array.java
@@ -4,7 +4,7 @@ public class multidimensional_array {
 	public static void main(String[] args) {
 		// 多次元配列
 		// 多次元配列とは、配列の各箱(要素)の中に
-	    // さらに小さな箱(要素)を入れて管理する方法
+		// さらに小さな箱(要素)を入れて管理する方法
 		
 		// 多次元配列　書き方 ①
 		// String[][] country  = 
